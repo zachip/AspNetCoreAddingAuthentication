3d4ee8b856cb5fac7111a3999af4272bb5039a70
Zach Hipsher <HipsherZ@bv.com>
2019-07-23 09:45:50 -0500

    Fixed Module 3 names

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -8,13 +8,13 @@ namespace WishList.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        private readonly UserManager<ApplicationUser> userManager;
-        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
         public AccountController(UserManager<ApplicationUser> user, SignInManager<ApplicationUser> signIn)
         {
-            userManager = user;
-            signInManager = signIn;
+            _userManager = user;
+            _signInManager = signIn;
         }
     }
 }
