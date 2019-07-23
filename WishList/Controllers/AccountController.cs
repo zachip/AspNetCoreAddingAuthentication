eb6c8b6e150a1936df96ab99b69b0bef085e9030
Zach Hipsher <HipsherZ@bv.com>
2019-07-23 09:44:36 -0500

    Module 3

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> user, SignInManager<ApplicationUser> signIn)
+        {
+            userManager = user;
+            signInManager = signIn;
+        }
+    }
+}
